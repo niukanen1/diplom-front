749c54c60400f66feaf770364ba8728ff1d00e3d
PetrTolstov <petr.tolstov@ivkhk.ee>
2023-05-18 19:02:39 +0300

    IVA-47 design profile page

--- pages/tabPages/ProfilePage/ProfilePageView.tsx
+++ pages/tabPages/ProfilePage/ProfilePageView.tsx
@@ -1,22 +1,53 @@
-import { Box, Button, Flex, Heading, Spacer, VStack, Text, Avatar } from "native-base";
+import {
+    Box,
+    Button,
+    Flex,
+    Heading,
+    Spacer,
+    VStack,
+    Text,
+    Avatar,
+} from "native-base";
 import { useSignOut } from "react-firebase-hooks/auth";
 import { auth } from "../../../Services/firebase/firebaseinit";
 
-export function ProfilePageView() { 
+export function ProfilePageView() {
     const [signOut, loading, error] = useSignOut(auth);
-    return ( 
-        <Flex justifyContent={'space-between'} h={'100%'}>
-            <Flex alignItems="center" padding={"25px"} borderBottomRightRadius={"50px"} height={"100px"}>
+    return (
+        <Flex justifyContent={"space-between"} h={"100%"}>
+            <Flex
+                direction={"row"}
+                alignItems="flex-start"
+                padding={"20px"}
+                paddingTop={"50px"}
+                borderBottomRightRadius={"50px"}
+                backgroundColor={"#1D8650"}
+            >
                 <Avatar
-                    size="lg"
-                    source={{ uri: 'URL_аватарки' }} // Замените 'URL_аватарки' на URL вашей аватарки
+                    borderColor={"#ffffff"}
+                    borderWidth={"6"}
+                    size="xl"
+                    source={{ uri: "URL_аватарки" }} // Замените 'URL_аватарки' на URL вашей аватарки
                 />
-                <Box backgroundColor={"#1D8650"} >
-                    <Text color={"white"}>Your email</Text>
-                    <Text color={"white"}>Full name</Text>
+                <Box paddingLeft={"15px"}>
+                    <Text color={"white"} fontSize={"26"} fontWeight={"600"}>
+                        Your email
+                    </Text>
+                    <Text color={"white"} fontSize={"18"} fontWeight={"400"}>
+                        Full name
+                    </Text>
                 </Box>
             </Flex>
-            <Button backgroundColor={'red.500'} onPress={() => {signOut()}} isLoading={loading}>Выйти</Button>
+            <Button
+                marginBottom={"10px"}
+                backgroundColor={"red.500"}
+                onPress={() => {
+                    signOut();
+                }}
+                isLoading={loading}
+            >
+                Выйти
+            </Button>
         </Flex>
-    )
+    );
 }
